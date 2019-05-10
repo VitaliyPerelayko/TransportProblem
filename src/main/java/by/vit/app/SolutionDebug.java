88f1c23cc2c8c202294d7ab04a189def8ef2db67
Vitaliy Perelayko <ant-1989aloe@mail.ru>
2019-05-10 17:05:29 +0300

    save

--- src/main/java/by/vit/app/SolutionDebug.java
+++ src/main/java/by/vit/app/SolutionDebug.java
@@ -1,6 +1,10 @@
 package by.vit.app;
 
+import by.vit.config.AppConfiguration;
 import by.vit.service.SolutionService;
+import by.vit.service.impl.SolutionServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,6 +17,15 @@ public class SolutionDebug {
 
 
     public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+
+        SolutionDebug solutionDebug = applicationContext.getBean("solutionServiceImpl",SolutionDebug.class);
+
+
+        solutionDebug.solutionService.setConditions();
+
+
+
 
     }
 }
---
+++ src/main/java/by/vit/config/AppConfiguration.java
@@ -0,0 +1,15 @@
+package by.vit.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+
+/**
+ * Base configuration of application
+ */
+@Configuration
+@ComponentScan(basePackages = "by.vit")
+@Import({DatabaseConfiguration.class, MessagesConfiguration.class})
+public class AppConfiguration {
+}
